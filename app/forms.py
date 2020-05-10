005fee68b7231492c0b0ac4a0c5a975013a2d0fd
Cargill Seiveright <microcargill@hotmail.com>
2020-05-10 17:18:55 -0500

    add form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,18 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Email
 
+class UserForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+    confirm_password = StringField('confirm_password', validators=[DataRequired()])
+    firstname = StringField('firstname', validators=[DataRequired()])
+    lastname = StringField('last_name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    location = StringField('location', validators=[DataRequired()])
+    biography = TextAreaField('biography', validators=[DataRequired()])
+    profile_photo = FileField('profile_photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png', 'jpeg', 'Images only!'])
+    ])
